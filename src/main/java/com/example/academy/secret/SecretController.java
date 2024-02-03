a1a1c1c518fce571b276dc2b222118a6f471c22d
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-02-03 13:23:06 +0100

    secret ok. ready to deploy

--- src/main/java/com/example/academy/secret/SecretController.java
+++ src/main/java/com/example/academy/secret/SecretController.java
@@ -21,39 +21,34 @@ public class SecretController {
 @Autowired
     UserService userService;
 @PostMapping("")
-@PreAuthorize("hasAuthority('ADMIN')")
     public Secret save(@RequestBody @Validated Secret secret, BindingResult validation){
     if(validation.hasErrors()){
         throw new BadRequestException(validation.getAllErrors());
     }
     List<Secret> secretList = this.getAll();
-    if(!secretList.isEmpty()){
+    if(secretList.isEmpty()){
         return secretService.save(secret);
     }else{
-        throw new BadRequestException("C'è già un secret salvato");
+        return this.updateById(secretList.get(0).getId(),secret);
     }
 }
 
 @GetMapping("")
-@PreAuthorize("hasAuthority('ADMIN')")
     public List<Secret> getAll(){
     return secretService.getAll();
 }
 
 @PutMapping("/{id}")
-@PreAuthorize("hasAuthority('ADMIN')")
     public Secret updateById(@PathVariable long id, @RequestBody Secret secret){
     return secretService.updateById(id,secret);
 }
 
 @DeleteMapping("/{id}")
-@PreAuthorize("hasAuthority('ADMIN')")
     public boolean deleteById(@PathVariable long id){
     return secretService.deleteById(id);
 }
 
 @PostMapping("/verify/{id}")
-@PreAuthorize("hasAuthority('ADMIN')")
 public boolean verifySecret(@PathVariable long id,@RequestBody Secret secret) {
 
     User user = userService.findById(id);
